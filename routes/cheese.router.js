863a1ea557a8da732099f801237bb5c466cb17c2
Adrian <adriantoddross@gmail.com>
2018-03-09 11:35:10 -0500

    return list of cheeses from

--- routes/cheese.router.js
+++ routes/cheese.router.js
@@ -3,6 +3,28 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/notes', (req, res, next) => {
+router.get('/cheeses', (req, res, next) => {
+
+  const cheeses = [
+    'Bath Blue',
+    'Barkham Blue',
+    'Buxton Blue',
+    'Cheshire Blue',
+    'Devon Blue',
+    'Dorset Blue Vinney',
+    'Dovedale',
+    'Exmoor Blue',
+    'Harbourne Blue',
+    'Lanark Blue',
+    'Lymeswold',
+    'Oxford Blue',
+    'Shropshire Blue',
+    'Stichelton',
+    'Stilton',
+    'Blue Wensleydale',
+    'Yorkshire Blue'
+  ];
+
+  return JSON.parse(cheeses);
 
 });
